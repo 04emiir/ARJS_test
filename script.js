d1a4ecac39e170818c762121209066c67a664b4a
04emiir <emilio.nero3@gmail.com>
2022-12-07 09:16:13 +0100

    nuevo4

--- script.js
+++ script.js
@@ -4,14 +4,34 @@ windows.onload = () => {
 }
 
 function cargarModelos() {
-	let latitud = 37.19226427531708;
-	let longitud = -3.6166251443071054;
+	//let latitud = 37.19226427531708;
+	//let longitud = -3.6166251443071054;
+	let modelos = [
+		{
+			latitud: 37.19224427531708,
+			longitud: -3.6166211443071054,
+			color: "yellow"
+		},
+		{
+			latitud: 37.19226427531708,
+			longitud: -3.6166251443071054,
+			color: "red"
+		},
+		{
+			latitud: 37.19226927531708,
+			longitud: -3.6166291443071054,
+			color: "green"
+		}
+	]
 
-	var modelo = document.createElement("a-box");
-	modelo.setAttribute('material', 'color:yellow');
-	modelo.setAttribute('scale', '10 10 10');
-	modelo.setAttribute('gps-entity-place', `latitude: ${latitud}; longitud: ${longitud}`);
-	let escena = document.querySelector('a-scene');
-	escena.appendChild(modelo);
+	for(let i = 0; i<modelos.length; i++ ) {
+		var modelo = document.createElement('a-box');
+		modelo.setAttribute('material', 'color:'+modelos[i.color);
+		modelo.setAttribute('scale', '10 10 10');
+		modelo.setAttribute('gps-entity-place', `latitude: ${modelos[i].latitud}; longitud: ${modelos[i].longitud};`);
+		let escena = document.querySelector('a-scene');
+		escena.appendChild(modelo);
+	}
+	
 
 } 
